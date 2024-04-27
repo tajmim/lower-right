246229d102f47214df29fe283916bc6de8de90a9
Shamima Tajmim <52608165+tajmim@users.noreply.github.com>
2024-04-27 17:17:37 +0600

    Mastering Pause

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,15 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>SocialV | Responsive Bootstrap 5 Admin Dashboard Template</title>
+
+    <link rel="shortcut icon" href="../assets/images/favicon.ico" />
+    <link rel="stylesheet" href="../assets/css/libs.min.css">
+    <link rel="stylesheet" href="../assets/css/socialv.css?v=4.0.0">
+    <link rel="stylesheet" href="../assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="../assets/vendor/remixicon/fonts/remixicon.css">
+    <link rel="stylesheet" href="../assets/vendor/vanillajs-datepicker/dist/css/datepicker.min.css">
+    <link rel="stylesheet" href="../assets/vendor/font-awesome-line-awesome/css/all.min.css">
+    <link rel="stylesheet" href="../assets/vendor/line-awesome/dist/line-awesome/css/line-awesome.min.css">
+
+</head>
